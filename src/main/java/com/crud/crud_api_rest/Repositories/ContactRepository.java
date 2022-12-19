bad0c527239f0a0009c0a9e094bc47106d72f3ff
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2022-12-19 12:05:38 -0300

    remove @Repository

--- src/main/java/com/crud/crud_api_rest/Repositories/ContactRepository.java
+++ src/main/java/com/crud/crud_api_rest/Repositories/ContactRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.crud.crud_api_rest.Models.Contact;
 
-@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
     List<Contact> findByNameContaining(String name);
